7cee79c7a35cda647895374a6eacf23906a95958
Reham <rehamhossam85@gmail.com>
2023-07-14 16:30:46 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main -  prints all single digit numbers two.
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int num;
+for (num = 0; num < 10; num++)
+{
+	putchar(num + '0');
+}
+putchar('\n');
+return (0);
+}
